672756d2272ff8b2573ec4fcd7e63e3f36820f57
Prawee Wongsa <prawee.w@integra8t.com>
2019-03-27 17:24:06 +0700

    create Landing components

---
+++ src/components/Landing.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: Prawee Wongsa prawee.w@integra8t.com 
+ * @Date: 2019-03-27 17:11:30 
+ * @Last Modified by: Prawee Wongsa
+ * @Last Modified time: 2019-03-27 17:13:00
+ */
+import React, { Component } from 'react'
+
+class Landing extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="jumbotron mt-5">
+                    <div className="col-sm-8 mx-auto">
+                        <h1 className="text-center">Welcome</h1>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Landing
