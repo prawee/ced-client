46d6e2d94747c0ff939e950626e016dae52d370b
Prawee Wongsa <prawee.w@integra8t.com>
2019-03-27 17:28:41 +0700

    fixed BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import { BorwerRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
